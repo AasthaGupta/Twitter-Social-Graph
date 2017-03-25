850cb96eebf5f3bb17a62080e3f963db16bb5e21
Aman Priyadarshi <aman.eureka@outlook.com>
2017-03-25 12:27:07 +0530

    fixed edge list

--- normalize.py
+++ normalize.py
@@ -2,7 +2,7 @@
 # @Author: amaneureka
 # @Date:   2017-03-25 08:41:51
 # @Last Modified by:   amaneureka
-# @Last Modified time: 2017-03-25 09:13:07
+# @Last Modified time: 2017-03-25 12:12:11
 
 import os
 import glob
@@ -14,6 +14,8 @@ def create_node_and_edge_list(inputfiles):
 	for filename in glob.glob(inputfiles):
 		node_a = os.path.splitext(os.path.basename(filename))[0]
 		nodes.add(node_a)
+		#if len(nodes) > 3000:
+		#	break
 		with open(filename, 'r') as file:
 			for line in file:
 				node_b = line[:-1]
@@ -21,9 +23,9 @@ def create_node_and_edge_list(inputfiles):
 				edges.append([node_a, node_b])
 	print 'dumping nodes...' + str(len(nodes))
 	with open('Datasets/nodes.txt', 'w') as file:
-		file.write('node\n')
+		file.write('Id;Label\n')
 		for node in nodes:
-			file.write('{}\n'.format(node))
+			file.write('{};{}\n'.format(node, node))
 	print 'dumping edges...' + str(len(edges))
 	with open('Datasets/edges.txt', 'w') as file:
 		file.write('Source;Target\n')
